4697b5ef70cb2b007ce1940d8d5eb58fdec1158f
eskainah <eskainah@gmail.com>
2024-01-31 17:56:32 +0200

    create sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let max = Math.max(num1, num2);
+    let min = Math.min(num1, num2);
+    let sum = 0;
+   
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if(min < 0 || max < 0) return "ERROR"
+    for(let i = min; i<=max; i++){
+            sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
