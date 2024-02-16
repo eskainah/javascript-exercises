3ee5110a31b77f5afb9f16e2990bbd76e3ee91ab
eskainah <eskainah@gmail.com>
2024-02-16 18:53:41 +0200

    write a function that checks for the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(pplDetail) {
+    return pplDetail.reduce((firstPerson, secondPerson) => {
+        const oldest = getAge(
+            firstPerson.yearOfBirth, 
+            firstPerson.yearOfDeath);
+        const youngest = getAge(
+            secondPerson.yearOfBirth, 
+            secondPerson.yearOfDeath
+        );
+        return oldest < youngest ? secondPerson : firstPerson;
+    });
+  };
+     
+const getAge = function (birth, death) {
+   if (!death) {
+     death = new Date().getFullYear();
+   }
+   return death - birth;
+ };
 
 // Do not edit below this line
 module.exports = findTheOldest;
