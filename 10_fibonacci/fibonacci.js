b2505c7345d881c357bada36855b1943868a8110
eskainah <eskainah@gmail.com>
2024-02-16 14:22:44 +0200

    write fuction to check fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    const fib = [0, 1];
+    if(input < 0) return "OOPS"
+    for (let i = 2; i <= input; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[input];
 };
 
 // Do not edit below this line
