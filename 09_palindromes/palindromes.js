3eb1ba5b748e9f2e8cf38d185652db62e807b590
eskainah <eskainah@gmail.com>
2024-02-16 14:10:29 +0200

    write function that checks string

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const processedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return processedStr.split("").reverse().join("") == processedStr;
 };
 
 // Do not edit below this line
