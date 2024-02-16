45fd89541adfa80b57279b1b6a7e9b99bfa8343b
eskainah <eskainah@gmail.com>
2024-02-16 14:02:28 +0200

    write all function for calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(inputs) {
+	return inputs.reduce((a,b)=> a + b, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(inputs) {
+ return inputs.reduce((a,b)=> a *b )
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+  if(input === 0) return 1;
+  let product = 1;
+  for(let i = input; i > 0; i--){
+    product *= i;
+  }
+  return product
 };
 
 // Do not edit below this line
